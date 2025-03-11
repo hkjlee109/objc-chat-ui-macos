d27f8f1239b5b629e8c484bf2e47c3be4b82e698
Kyung <hkjlee109@gmail.com>
2025-03-11 16:17:12 +0100

    Add TextView expand logic.

--- objc-chat-ui-macos/ViewController.m
+++ objc-chat-ui-macos/ViewController.m
@@ -1,6 +1,12 @@
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <NSTextViewDelegate>
+
+@property (weak) IBOutlet NSScrollView *chatInputScrollView;
+@property (unsafe_unretained) IBOutlet NSTextView *chatTextView;
+@property (weak) IBOutlet NSButton *sendButton;
+
+@property (weak) IBOutlet NSLayoutConstraint *chatInputScrollViewHeight;
 
 @end
 
@@ -8,6 +14,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.chatTextView.delegate = self;
+    
+    NSImage *image = [NSImage imageWithSystemSymbolName:@"paperplane.fill" accessibilityDescription:@"Send"];
+    [self.sendButton setImage:image];
+    
+    
+    self.chatInputScrollView.wantsLayer = YES;
+    self.chatInputScrollView.layer.cornerRadius = 8.0;
+    self.chatInputScrollView.layer.masksToBounds = YES;
+    self.chatInputScrollView.hasVerticalScroller = NO;
+
+    self.chatTextView.textContainerInset = NSMakeSize(0, (self.chatTextView.bounds.size.height - self.chatTextView.font.pointSize) / 2);
+}
+
+- (void)textDidChange:(NSNotification *)notification {
+    CGFloat contentHeight = [self.chatTextView.layoutManager usedRectForTextContainer:self.chatTextView.textContainer].size.height;
+    
+    CGFloat minHeight = 32.0;
+    CGFloat maxHeight = 82.0;
+    CGFloat newHeight = fmin(fmax(contentHeight + 18, minHeight), maxHeight);
+
+    self.chatInputScrollViewHeight.constant = newHeight;
 }
 
 @end
