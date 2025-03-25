adaf196b064fbbbeca1eecbf553a617bf42d8223
Kyung <hkjlee109@gmail.com>
2025-03-25 14:39:28 +0100

    Remove unused.

--- objc-chat-ui-macos/ChatInputTextField.h
+++ objc-chat-ui-macos/ChatInputTextField.h
@@ -4,10 +4,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ChatInputTextField : NSTextField
 
-@property (nonatomic, copy) void (^onEnterKeyPress)(void);
-
-- (NSSize)intrinsicContentSize;
-
 @end
 
 NS_ASSUME_NONNULL_END
